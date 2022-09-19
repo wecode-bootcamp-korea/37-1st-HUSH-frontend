6c06795083f9b387e10525128f0ba4283f01dab6
kimdaeho <eogh773@naver.com>
2022-09-19 18:10:07 +0900

    최종 초기세팅

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Test from './pages/Deaho/Test'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
+import Join from "./pages/Join/Join";
+import Detail from "./pages/detailPage/Detail";
+import Login from "./pages/Login/Login";
+import MyPage from "./pages/myPage/MyPage";
+import PayPage from "./pages/payPage/PayPage";
+import Product from "./pages/ProductPage/Product";
+import Main from "./pages/main/Main";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test />}></Route>
+        <Route path="/" element={<Main />} />
+        <Route path="/nav" element={<Nav />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/paypage" element={<PayPage />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/payPage/PayPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PayPage() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PayPage;
