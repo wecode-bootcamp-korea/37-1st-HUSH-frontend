29e730f3dfa0eb14cf10bb7305b0b825ed1a0690
sean <a900906@naver.com>
2022-09-22 13:50:02 +0900

    Add: 필터링구현중

--- src/pages/ProductPage/ProductChocolate.js
+++ src/pages/ProductPage/ProductChocolate.js
@@ -2,29 +2,16 @@ import React from 'react';
 
 const ProductChocolate = ({ product }) => {
   const { name, img_id, price, category_id } = product;
-  // const { name1, price1, category_id1 } = product;
-  // const { name2, price2, category_id2 } = product;
-  // const { name3, price3, category_id3 } = product;
 
   return (
-    <div className="product-product-outer-box">
-      <div className="product-product-box">
-        <div className="product-product-inner-box">
-          <div className="product-product-image-box">
-            <div className="thumb">
-              <img
-                className="product-product-image"
-                src={img_id}
-                alt="상품이미지"
-              />
-            </div>
-            <div className="product-product-info">
-              <p className="bold">{name}</p>
-              <p>{category_id}</p>
-              <p>{price}</p>
-            </div>
-          </div>
-        </div>
+    <div className="product-product-inner-box">
+      <div className="thumb">
+        <img className="product-product-image" src={img_id} alt="상품이미지" />
+      </div>
+      <div className="product-product-info">
+        <p className="bold">{name}</p>
+        <p>{category_id}</p>
+        <p>{price}</p>
       </div>
     </div>
   );
