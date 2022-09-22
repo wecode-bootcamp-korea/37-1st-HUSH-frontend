3dbcfa49f26df2d174d8a08dbb512c725e55e57f
Hyerin Joo <candy980426@gmail.com>
2022-09-22 12:57:44 +0900

    Modify : alert 문구 수정

--- src/pages/Join/Join.js
+++ src/pages/Join/Join.js
@@ -79,12 +79,12 @@ function Join() {
         })
         .then(data => {
           if (data.message === 'SIGN_UP_SUCCESS') {
-            alert('회원가입 성공');
+            alert('회원가입 성공!');
             navigate('/login');
           } else if (data.message === 'INVALID_PASSWORD') {
-            alert('회원가입 실패');
-          } else {
-            alert('회원가입 실패');
+            alert(
+              '영문(대소문자)과 특수문자를 포함한 8자리 이상의 비밀번호를 작성해주세요.'
+            );
           }
         });
     } else {
