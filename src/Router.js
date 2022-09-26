c32e2dad57baa51e72b63b58f79be429c19b4cba
kimdaeho <eogh773@naver.com>
2022-09-26 13:04:29 +0900

    common 9/26 commit

--- src/Router.js
+++ src/Router.js
@@ -1,29 +1,37 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/nav/Nav";
-import Footer from "./components/footer/Footer";
-import Join from "./pages/Join/Join";
-import Detail from "./pages/detailPage/Detail";
-import Login from "./pages/Login/Login";
-import MyPage from "./pages/myPage/MyPage";
-import PayPage from "./pages/payPage/PayPage";
-import Product from "./pages/ProductPage/Product";
-import Main from "./pages/main/Main";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/nav/Nav';
+import Footer from './components/footer/Footer';
+import Join from './pages/Join/Join';
+import Detail from './pages/detailPage/Detail';
+import Login from './pages/Login/Login';
+import PayPage from './pages/payPage/PayPage';
+import Product from './pages/ProductPage/Product';
+import Main from './pages/main/Main';
+import Cart from './pages/cart/Cart';
+import Event from './pages/sub/Event';
+import ScrollToTop from './ScrollToTop';
+import Intro from './pages/sub/Intro';
+import Store from './pages/sub/Store';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/nav" element={<Nav />} />
-        <Route path="/footer" element={<Footer />} />
         <Route path="/join" element={<Join />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/mypage" element={<MyPage />} />
         <Route path="/paypage" element={<PayPage />} />
         <Route path="/product" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="event" element={<Event />} />
+        <Route path="intro" element={<Intro />} />
+        <Route path="store" element={<Store />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
---
+++ src/pages/sub/Intro.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Intro.scss';
+
+function Intro() {
+  return (
+    <div className="Intro-wrap">
+      <video
+        width="440"
+        height="250"
+        className="event-video"
+        autoPlay="outoplay"
+        loop="Loop"
+        muted
+      >
+        <source src="/images/Nav/test.mp4" />
+      </video>
+      <strong>준비중인 페이지입니다.</strong>
+    </div>
+  );
+}
+
+export default Intro;
