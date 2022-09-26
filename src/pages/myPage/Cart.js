217655954a9f24226ddf20630666d455e1d70f7f
Hyerin Joo <candy980426@gmail.com>
2022-09-26 21:08:18 +0900

    Refactor : 불필요한 내용 삭제

--- src/pages/myPage/Cart.js
+++ src/pages/myPage/Cart.js
@@ -4,9 +4,11 @@ import '../myPage/Cart.scss';
 
 function Cart() {
   const [productData, setProductData] = useState([]);
+  const [checkedList, setCheckedList] = useState([]);
+  const [quantityData, setQuantityData] = useState({});
 
   useEffect(() => {
-    fetch('http://192.168.228.252:3000/cart/listUp', {
+    fetch('/data/cart.json', {
       headers: {
         authorization:
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNjYzOTE2MjQ3fQ.au4JgHfu9_Js-l3eaPHyh-UrsAGQ1Wily3XSKh3VzH4',
@@ -22,12 +24,9 @@ function Cart() {
         alert(error);
       })
       .then(data => {
-        console.log(data.data);
-        setProductData(data.data);
+        setProductData(data);
       });
   }, []);
-
-  const [checkedList, setCheckedList] = useState([]);
 
   const handleSingleChecked = e => {
     if (e.target.checked) {
@@ -47,10 +46,12 @@ function Cart() {
     }
   };
 
-  const [quantityData, setQuantityData] = useState({});
-
   const changeQuantity = (pId, key, value) => {
-    setQuantityData({ ...quantityData, pId: pId, quantity: value });
+    setQuantityData({
+      ...quantityData,
+      pId: pId,
+      quantity: value,
+    });
   };
 
   const deleteChecked = () => {
@@ -58,6 +59,7 @@ function Cart() {
       return !checkedList.includes(el.pId);
     });
     setProductData(filtered);
+    setCheckedList([]);
   };
 
   const orderProduct = () => {
@@ -73,76 +75,74 @@ function Cart() {
   };
 
   return (
-    <>
-      <div className="cart">
-        <h1 className="cart-title">장바구니</h1>
-        <div className="cart-subTitle">일반배송</div>
-        <table className="cart-product">
-          <thead className="cart-product-head">
-            <tr>
-              <th>
-                <input
-                  className="head-checkbox"
-                  type="checkbox"
-                  id="checkbox"
-                  checked={checkedList.length === productData.length}
-                  onChange={handleAllChecked}
-                />
-                <label htmlFor="checkbox" />
-              </th>
-              <th className="head-text-info">제품 정보</th>
-              <th className="head-text">수량</th>
-              <th className="head-text">금액</th>
-              <th className="head-text-total">합계금액</th>
-            </tr>
-          </thead>
-          <tbody className="cart-product-body">
-            {productData.map(product => (
-              <CartProduct
-                key={product.pId}
-                id={product.pId}
-                img={product.url}
-                name={product.pName}
-                category={product.cName}
-                quantity={product.quantity}
-                price={product.price}
-                stock={product.pStock}
-                handleSingleChecked={handleSingleChecked}
-                checkedList={checkedList}
-                changeQuantity={changeQuantity}
+    <div className="cart">
+      <h1 className="cart-title">장바구니</h1>
+      <div className="cart-subTitle">일반배송</div>
+      <table className="cart-product">
+        <thead className="cart-product-head">
+          <tr>
+            <th>
+              <input
+                className="head-checkbox"
+                type="checkbox"
+                id="checkbox"
+                checked={checkedList.length === productData.length}
+                onChange={handleAllChecked}
               />
-            ))}
-          </tbody>
-        </table>
-        <button className="cart-delete-btn" onClick={deleteChecked}>
-          선택 삭제
+              <label htmlFor="checkbox" />
+            </th>
+            <th className="head-text-info">제품 정보</th>
+            <th className="head-text">수량</th>
+            <th className="head-text">금액</th>
+            <th className="head-text-total">합계금액</th>
+          </tr>
+        </thead>
+        <tbody className="cart-product-body">
+          {productData.map(product => (
+            <CartProduct
+              key={product.pId}
+              id={product.pId}
+              img={product.url}
+              name={product.pName}
+              category={product.cName}
+              quantity={product.quantity}
+              price={product.price}
+              stock={product.pStock}
+              handleSingleChecked={handleSingleChecked}
+              checkedList={checkedList}
+              changeQuantity={changeQuantity}
+            />
+          ))}
+        </tbody>
+      </table>
+      <button className="cart-delete-btn" onClick={deleteChecked}>
+        선택 삭제
+      </button>
+      <ul className="cart-calc">
+        <li>
+          <span className="calc-title">선택제품</span>
+          <span className="calc-count">{checkedList.length} 개</span>
+        </li>
+        <li>
+          <span className="calc-title">제품합계</span>
+          <span className="calc-sum">₩ 0</span>
+        </li>
+        <li>
+          <span className="calc-title cal-title-shift">배송비</span>
+          <span className="calc-sum">무료</span>
+        </li>
+        <li>
+          <span className="calc-title">주문금액</span>
+          <span className="calc-sum">₩ 0</span>
+        </li>
+      </ul>
+      <div>
+        <button className="cart-shop-btn">쇼핑 계속하기</button>
+        <button className="cart-pay-btn" onClick={orderProduct}>
+          주문하기
         </button>
-        <ul className="cart-calc">
-          <li>
-            <span className="calc-title">선택제품</span>
-            <span className="calc-count">{checkedList.length} 개</span>
-          </li>
-          <li>
-            <span className="calc-title">제품합계</span>
-            <span className="calc-sum">₩ 0</span>
-          </li>
-          <li>
-            <span className="calc-title cal-title-shift">배송비</span>
-            <span className="calc-sum">무료</span>
-          </li>
-          <li>
-            <span className="calc-title">주문금액</span>
-            <span className="calc-sum">₩ 0</span>
-          </li>
-        </ul>
-        <div>
-          <button className="cart-shop-btn">쇼핑 계속하기</button>
-          <button className="cart-pay-btn" onClick={orderProduct}>
-            주문하기
-          </button>
-        </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/pages/myPage/CartProduct.js
+++ src/pages/myPage/CartProduct.js
@@ -12,14 +12,11 @@ function CartProduct({
   handleSingleChecked,
   checkedList,
   changeQuantity,
-  totalSum,
+  handleTotalSum,
 }) {
-  price = Number(price);
-  quantity = Number(quantity);
-
   const [productQuantity, setProductQuantity] = useState(quantity);
 
-  totalSum = productQuantity * price;
+  let totalSum = productQuantity * price;
 
   const decreaseQuantity = e => {
     if (productQuantity <= 1) {
@@ -27,9 +24,11 @@ function CartProduct({
     } else {
       setProductQuantity(productQuantity - 1);
       // fetch('api 주소', {
-      //   method: 'POST',
-      //   body:JSON.stringify()
-      // })
+      //   method: 'PATCH',
+      //   headers: {
+      //     authorization: '',
+      //   },
+      // });
     }
   };
 
@@ -38,44 +37,48 @@ function CartProduct({
       alert(`최대 구매가능 수량은 ${stock}개입니다.`);
     } else {
       setProductQuantity(productQuantity + 1);
+      // fetch('api 주소', {
+      //   method: 'PATCH',
+      //   headers: {
+      //     authorization: '',
+      //   },
+      // });
     }
   };
 
   useEffect(() => {
-    changeQuantity(id, 'quantity', productQuantity);
+    changeQuantity(id, 'quantity', setProductQuantity);
   }, [productQuantity]);
 
   return (
-    <>
-      <tr className="cart-product-content" id={id}>
-        <td className="product-content-input">
-          <input
-            type="checkbox"
-            id={id}
-            checked={checkedList.includes(id)}
-            onChange={handleSingleChecked}
-          />
-        </td>
-        <td className="product-content-detail">
-          <img className="content-detail-img" src={img} alt="제품 이미지" />
-          <div>
-            <p className="content-detail-name">{name}</p>
-            <p className="content-detail-category">{category}</p>
-          </div>
-        </td>
-        <td className="product-content-quantity">
-          <button className="quantity-minus-btn" onClick={decreaseQuantity} />
-          <span className="quantity-count">{productQuantity}</span>
-          <button className="quantity-plus-btn" onClick={increaseQuantity} />
-        </td>
-        <td className="product-content-price">
-          ₩ {price.toLocaleString('ko-KR')}
-        </td>
-        <td className="product-content-totalPrice">
-          ₩ {totalSum.toLocaleString('ko-KR')}
-        </td>
-      </tr>
-    </>
+    <tr className="cart-prosduct-content" id={id}>
+      <td className="product-content-input">
+        <input
+          type="checkbox"
+          id={id}
+          checked={checkedList.includes(id)}
+          onChange={handleSingleChecked}
+        />
+      </td>
+      <td className="product-content-detail">
+        <img className="content-detail-img" src={img} alt="제품 이미지" />
+        <div>
+          <p className="content-detail-name">{name}</p>
+          <p className="content-detail-category">{category}</p>
+        </div>
+      </td>
+      <td className="product-content-quantity">
+        <button className="quantity-minus-btn" onClick={decreaseQuantity} />
+        <span className="quantity-count">{productQuantity}</span>
+        <button className="quantity-plus-btn" onClick={increaseQuantity} />
+      </td>
+      <td className="product-content-price">
+        ₩ {price.toLocaleString('ko-KR')}
+      </td>
+      <td className="product-content-totalPrice">
+        ₩ {totalSum.toLocaleString('ko-KR')}
+      </td>
+    </tr>
   );
 }
 
