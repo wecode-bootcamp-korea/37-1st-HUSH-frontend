275c99203741909ba63bde033f5238563370f93b
Hyerin Joo <candy980426@gmail.com>
2022-09-26 17:43:00 +0900

    Fix: 리뷰 반영

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
     email: '',
     password: '',
@@ -25,62 +27,50 @@ function Login() {
         password: password,
       }),
     })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('에러 발생!');
-      })
-      .catch(error => {
-        if (error.message === 'WRONG_EMAIL') {
-          alert('이메일을 다시 작성해주세요.');
-        } else if (error.message === 'WRONG_PASSWORD') {
-          alert('비밀번호를 다시 작성해주세요.');
-        }
-      })
+      .then(response => response.json())
       .then(data => {
         if (data.message === 'EXCESS_SUCCESS') {
           localStorage.setItem('accessToken', data.accessToken);
           alert('로그인 성공!');
-        } else {
-          alert('로그인 실패!');
+          navigate('/');
+          return;
         }
+
+        if (ERROR_MSG[data.message]) {
+          alert(ERROR_MSG[data.message]);
+          return;
+        }
+
+        alert('로그인 실패!');
       });
   };
 
   return (
-    <>
-      <div className="login">
-        <h1 className="login-title">로그인</h1>
-        <form
-          className="login-form"
-          onChange={inputChange}
-          onSubmit={submitForm}
-        >
-          <input
-            className="login-email-input"
-            name="email"
-            placeholder="이메일"
-          />
-          <input
-            className="login-password-input"
-            name="password"
-            placeholder="비밀번호"
-          />
-          <input
-            type="checkbox"
-            id="saveEmail"
-            className="login-checkbox-input"
-          />
-          <label htmlFor="saveEmail" className="login-checkbox-label">
-            <span>아이디 저장</span>
-          </label>
-          <button className="login-btn">로그인</button>
-        </form>
-        <p className="login-link-join">회원가입</p>
-      </div>
-    </>
+    <div className="login">
+      <h1 className="login-title">로그인</h1>
+      <form className="login-form" onChange={inputChange} onSubmit={submitForm}>
+        <input className="login-input" name="email" placeholder="이메일" />
+        <input className="login-input" name="password" placeholder="비밀번호" />
+        <input
+          type="checkbox"
+          id="saveEmail"
+          className="login-checkbox-input"
+        />
+        <label htmlFor="saveEmail" className="login-checkbox-label">
+          <span>아이디 저장</span>
+        </label>
+        <button className="login-btn">로그인</button>
+      </form>
+      <p className="login-link-join" onClick={() => navigate('/join')}>
+        회원가입
+      </p>
+    </div>
   );
 }
 
 export default Login;
+
+const ERROR_MSG = {
+  WRONG_EMAIL: '이메일을 다시 작성해주세요.',
+  WRONG_PASSWORD: '비밀번호를 다시 작성해주세요.',
+};
