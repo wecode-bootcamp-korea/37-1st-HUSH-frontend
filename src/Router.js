f63594f9dc1a6db36658f8c00ecce696b3046a6c
kimdaeho <eogh773@naver.com>
2022-09-29 15:32:49 +0900

    common 9/29 last commit

--- src/Router.js
+++ src/Router.js
@@ -8,11 +8,12 @@ import Login from './pages/Login/Login';
 import PayPage from './pages/payPage/PayPage';
 import Product from './pages/ProductPage/Product';
 import Main from './pages/main/Main';
-import Cart from './pages/cart/Cart';
+import Cart from './pages/Cart/Cart';
 import Event from './pages/sub/Event';
 import ScrollToTop from './ScrollToTop';
 import Intro from './pages/sub/Intro';
 import Store from './pages/sub/Store';
+import Like from './pages/Like/Like';
 
 const Router = () => {
   return (
@@ -30,10 +31,10 @@ const Router = () => {
         <Route path="event" element={<Event />} />
         <Route path="intro" element={<Intro />} />
         <Route path="store" element={<Store />} />
+        <Route path="like" element={<Like />} />
       </Routes>
       <Footer />
     </BrowserRouter>
   );
 };
 export default Router;
-
--- src/pages/sub/Intro.js
+++ src/pages/sub/Intro.js
@@ -9,7 +9,7 @@ function Intro() {
         <div className="team-content-wrap">
           <img
             className="intro-team-img"
-            src="/images/sub/team.jpg"
+            src="/images/sub/team.jpeg"
             alt="팀 이미지"
           />
           <p className="team-deatil">HUSH팀 최고최고 우후후후훟후훟 야야야야</p>
@@ -47,36 +47,33 @@ const INTRO_CONTENT = [
     id: 1,
     title: '회원가입 페이지',
     img: '/images/sub/01.회원가입페이지.png',
-    detail:
-      'slide, hover 등 화려한 애니메이션 효과를 바탕으로 프로젝트의 시작페이지',
+    detail: '회원가입 시 id중복확인 및 유효성 검사를 통한 계정 생성 페이지',
   },
   {
     id: 2,
     title: '로그인 페이지',
     img: '/images/sub/02.로그인페이지.png',
     detail:
-      'slide, hover 등 화려한 애니메이션 효과를 바탕으로 프로젝트의 시작페이지',
+      '유효성 검사 및 실제 DB에 저장된 데이터를 조회하여 로그인 기능 수행, 로그인 시 토큰과 포인트를 발급',
   },
   {
     id: 3,
     title: '제품 페이지',
     img: '/images/sub/03.제품페이지.png',
     detail:
-      'slide, hover 등 화려한 애니메이션 효과를 바탕으로 프로젝트의 시작페이지',
+      'Hush의 컨셉인 디저트라는 것에 맞추어, chocolate, candy, jelly, cookie, cake의 5개의 카테고리로 디저트를 소개하는 페이지',
   },
   {
     id: 4,
     title: '상세 페이지',
     img: '/images/sub/05.상세페이지.png',
-    detail:
-      'slide, hover 등 화려한 애니메이션 효과를 바탕으로 프로젝트의 시작페이지',
+    detail: '제품의 상세 정보 및 리뷰 등을 조회할 수 있는 페이지',
   },
   {
     id: 5,
-    title: '장바구니 페이지',
+    title: '장바구니 페이지 & 찜 페이지',
     img: '/images/sub/04.장바구니페이지.png',
-    detail:
-      'slide, hover 등 화려한 애니메이션 효과를 바탕으로 프로젝트의 시작페이지',
+    detail: '구매하고자 하는 상품을 결제페이지 이동 전 확인하는 페이지',
   },
 
   {
@@ -84,6 +81,6 @@ const INTRO_CONTENT = [
     title: '결제 페이지',
     img: '/images/sub/06.결제페이지.png',
     detail:
-      'slide, hover 등 화려한 애니메이션 효과를 바탕으로 프로젝트의 시작페이지',
+      '구매 절차를 통한 제품을 최종으로 확인하고 결제할 수 있는 페이지, 결제 시 포인트 차감 실시간 반영',
   },
 ];
